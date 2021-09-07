c519aa49abfa58882b2a775231c80bc613979c9f
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2021-09-07 15:31:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* If there is an error in the code, Bugs, Visual glitches and other stuff, Just select this area and click "Create thread" and address your issue. */
+
+const app = require('express')
+
+const middleware = (req, res, next) => {
+
+} 
+app.use(middleware)
